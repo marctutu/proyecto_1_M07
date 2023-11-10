5c542c39913b8af00e1e11bd70ddc8af0387103d
Marctutu5 <marctutu5@gmail.com>
2023-11-10 20:32:39 +0100

    Error migrations solucionado Versió final 0.1.9

--- laravel/database/migrations/2023_11_02_173836_create_posts_table.php
+++ laravel/database/migrations/2023_11_02_173836_create_posts_table.php
@@ -4,28 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('file_id');
-            $table->string('body', 255);  // Añadir columna para el cuerpo del post
-            $table->foreign('file_id')->references('id')->on('files');
-            $table->timestamps();  // Esto ya añade las columnas created_at y updated_at
+            $table->id(); // bigint(20) y autoincremento
+            $table->string('body', 255); // varchar(255)
+            $table->unsignedBigInteger('file_id')->nullable(); // bigint(20), nullable
+            $table->float('latitude'); // float
+            $table->float('longitude'); // float
+            $table->unsignedBigInteger('author_id'); // bigint(20)
+            $table->timestamps(); // timestamp con valor por defecto al tiempo actual
+            // Si necesitas también la columna updated_at, puedes usar $table->timestamps();
         });
     }
-
-
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('posts');
     }
