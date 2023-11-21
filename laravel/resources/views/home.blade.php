11a33b553d38e15fa1c845c520bc7813a5deb031
Its-Draky <axviro@fp.insjoaquimmir.cat>
2023-11-21 19:49:09 +0100

    va el forelse de las imagenes

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -43,19 +43,29 @@
          <div class="w-2/6 pl-8 border-r-2 border-black">
             <!-- Fila 1 -->
             <div class="flex mb-4 h-1/3">
-               <div class="w-1/4">
-                  <img src='{{ asset("storage/{$imagen->filepath}") }}' alt="Imagen 1" class="w-full h-full">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen2.jpg" alt="Imagen 2" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen3.jpg" alt="Imagen 3" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen4.jpg" alt="Imagen 4" class="w-full h-auto">
-               </div>    
-            </div>
+               @forelse ($posts as $post)
+                                <div class="w-1/4">
+                                    
+                                    <div class="h-5/6>
+                                        @if($post->file)
+                                            <div class="mt-4">
+                                                <img src='{{ asset("storage/{$post->file->filepath}") }}' alt="File Image" class="w-32 h-32 object-cover mb-2">
+                                        @endif
+                                    </div>
+                                    <div class="h-1/6">
+                                        {{ strlen($post->body) > 20 ? substr($post->body, 0, 20) . '...' : $post->body }}
+                                        @if(strlen($post->body) > 20)
+                                            <a href="{{ route('posts.show', $post->id) }}" class="text-indigo-600 hover:text-indigo-900">Ver más</a>
+                                        @endif
+                                    </div>
+                                    <div class="px-6 py-4 whitespace-nowrap">{{ $post->author->name }}</div>
+                                 </div>
+                            @empty
+                                <tr>
+                                    <td colspan="8" class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">No hay posts disponibles.</td>
+                                </tr>
+                  @endforelse
+            </div> 
 
             <!-- Fila 2 -->
             <div class="flex mb-4 h-1/3">
