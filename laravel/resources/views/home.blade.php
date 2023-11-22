3edb657ea0106b3ef1af8a7ada455cdc4bb2ff54
Its-Draky <axviro@fp.insjoaquimmir.cat>
2023-11-22 19:17:28 +0100

    funciona carrusel imatges

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,54 +1,51 @@
 <!-- resources/views/geomir.blade.php -->
 <x-geomir-layout>
    @section('content')
-      <div class="bg-white">
+      <div class="h-screen bg-white">
 
-<!-- Menú superior con línea horizontal -->
+         <!-- Menú superior con línea horizontal -->
+         
          <div class="bg-white-500 text-black p-4 py-6 relative">
-         <div class="container mx-auto flex justify-center items-center">
-            <div class="font-bold">Logo</div>
-         </div>
-         <!-- Línea horizontal -->
-         <hr class="absolute bottom-0 left-5 right-5 border-t-2 border-black rounded-full">
+            <div class="container mx-auto flex justify-center items-center">
+               <div class="font-bold">Logo</div>
+            </div>
+
+            <!-- Línea horizontal -->
+
+            <hr class="absolute bottom-0 left-5 right-5 border-t-2 border-black rounded-full">
          </div>
 
          <!-- Segundo menú con línea horizontal y desplazamiento hacia abajo -->
 
          <div class="bg-white-300 p-4 py-6 relative">
-         <div class="container mx-auto px-10 flex justify-between">
-            <a href="#" class="px-4 hover:underline">Home</a>
-            <a href="#" class="px-4 hover:underline">Explore</a>
-            <a href="#" class="px-4 hover:underline">Chat</a>
-            <a href="#" class="px-4 hover:underline">New post</a>
-            <a href="#" class="px-4 hover:underline">New Place</a>
-         </div>
+            <div class="container mx-auto px-10 flex justify-between">
+               <a href="#" class="px-4 hover:underline">Home</a>
+               <a href="#" class="px-4 hover:underline">Explore</a>
+               <a href="#" class="px-4 hover:underline">Chat</a>
+               <a href="#" class="px-4 hover:underline">New post</a>
+               <a href="#" class="px-4 hover:underline">New Place</a>
+            </div>
 
-         <!-- Línea horizontal -->
+            <!-- Línea horizontal -->
 
-         <hr class="absolute bottom-0 left-0 right-0 border-t-2 border-black rounded-full mt-5">
+            <hr class="absolute bottom-0 left-0 right-0 border-t-2 border-black rounded-full mt-5">
          </div>
 
          <!-- Contenido principal -->
 
-         <div class="h-screen w-full p-4 py-6 relative flex">
+         <div class="w-full p-4 py-6 relative flex">
 
-         <!-- Imagen a la izquierda -->
+            <!-- Imagen a la izquierda -->
 
-         <div class="w-3/6 relative border-r-2 border-black">
-            <img src="tu_imagen.jpg" alt="Imagen Izquierda" class="w-full h-auto">
-         </div>
-
-         <!-- Contenedor de imágenes a la derecha -->
-
-         <div class="w-2/6 pl-8 border-r-2 border-black">
-            <!-- Fila 1 -->
-            <div class="flex mb-4 h-1/3">
-               @forelse ($posts as $post)
-                        <div class="w-1/4">  
-                           <div >
+            <div class="relative border-r-2 border-black" style="width: 40%;">
+               <div id="post-container" class="post-container">
+                  @forelse ($posts as $index => $post)
+                     <div class="post-slide" style="display: {{ $index === 0 ? 'block' : 'none' }}">
+                           <!-- Contenido de la publicación -->
+                           <div>
                               @if($post->file)
                                  <div class="relative">
-                                       <img src='{{ asset("storage/{$post->file->filepath}") }}' alt="File Image" class="w-32 h-32">
+                                       <img src='{{ asset("storage/{$post->file->filepath}") }}' alt="File Image" class="w-44 h-40">
                                        <div class="absolute top-0 left-0 bg-white p-2">
                                           <p>{{ $post->author->name }}</p>
                                        </div>
@@ -56,64 +53,88 @@
                               @endif
                            </div>
                            <div>
-                                 {{ strlen($post->body) > 20 ? substr($post->body, 0, 20) . '...' : $post->body }}
-                                 @if(strlen($post->body) > 20)
-                                    <a href="{{ route('posts.show', $post->id) }}" class="text-indigo-600 hover:text-indigo-900">Ver más</a>
-                                 @endif
+                              <a href="{{ route('posts.show', $post->id) }}">{{ strlen($post->body) > 20 ? substr($post->body, 0, 20) . '...' : $post->body }}</a>
+                           </div>
+                     </div>
+                  @empty
+                     <div class="w-full px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">No hay posts disponibles.</div>
+                  @endforelse
+
+                  <!-- Botones de navegación -->
+                  <button id="prev-post" onclick="changePost(-1)">Anterior</button>
+                  <button id="next-post" onclick="changePost(1)">Siguiente</button>
+               </div>
+            </div>
+
+            <!-- Contenedor de imágenes a la derecha -->
+
+            <div class="pl-8 border-r-2 border-black" style="width: 50%;">
+               <!-- Fila 1 -->
+                  <div class="flex flex-wrap">
+                     @forelse ($posts as $index => $post)
+                        <div class="w-1/4 mb-4">
+                           <div>
+                              @if($post->file)
+                                 <div class="relative">
+                                       <img src='{{ asset("storage/{$post->file->filepath}") }}' alt="File Image" class="w-44 h-40">
+                                       <div class="absolute top-0 left-0 bg-white p-2">
+                                          <p>{{ $post->author->name }}</p>
+                                       </div>
+                                 </div>
+                              @endif
+                           </div>
+                           <div>
+                              <a href="{{ route('posts.show', $post->id) }}">{{ strlen($post->body) > 20 ? substr($post->body, 0, 20) . '...' : $post->body }}</a>
                            </div>
                         </div>
+               @if(($index + 1) % 4 == 0)
+                  </div>
+                  <div class="flex flex-wrap">
+               @endif
                      @empty
-                        <tr>
-                           <td colspan="8" class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">No hay posts disponibles.</td>
-                        </tr>
-                  @endforelse
-            </div> 
-
-            <!-- Fila 2 -->
-            <div class="flex mb-4 h-1/3">
-               <div class="w-1/4">
-                  <img src="imagen1.jpg" alt="Imagen 5" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen2.jpg" alt="Imagen 6" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen3.jpg" alt="Imagen 7" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen4.jpg" alt="Imagen 8" class="w-full h-auto">
-               </div>   
+                        <div class="w-full px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">No hay posts disponibles.</div>
+                     @endforelse
+                  </div>
             </div>
-            
-            <div class="flex mb-4 h-1/3">
-               <div class="w-1/4">
-                  <img src="imagen1.jpg" alt="Imagen 9" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen2.jpg" alt="Imagen 10" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen3.jpg" alt="Imagen 11" class="w-full h-auto">
-               </div>
-               <div class="w-1/4 pl-4">
-                  <img src="imagen4.jpg" alt="Imagen 12" class="w-full h-auto">
-               </div>   
-            </div>
-         </div>
-         <div class="w-1/6 pl-8 ">
-               <div class="container px-10 flex-col">
-                  <a href="#" class="block py-2 hover:underline">Likes</a>
-                  <a href="#" class="block py-2 hover:underline">Account</a>
-                  <a href="#" class="block py-2 hover:underline">Favs</a>
+            <div class="pl-8" style="width: 10%;">
+               <div class="container px-10 flex-col space-y-40">
+                  <div class="pt-10">
+                     <a href="#" class="block py-2 hover:underline">Likes</a>
+                  </div>
+                  <div>
+                     <a href="#" class="block py-2 hover:underline">Account</a>
+                  </div>
+                  <div class="pb-10">
+                     <a href="#" class="block py-2 hover:underline">Favs</a>
+                  </div>
+                  
                </div>
             </div>
-         </div>
-
-         <!-- Línea de delimitación -->
-         <hr class="my-8">
          </div>
       </div>
-    @endsection
+      <script>
+         // Índice de la publicación actual
+         let currentPostIndex = 0;
+
+         // Función para cambiar de publicación
+         function changePost(direction) {
+            const posts = document.querySelectorAll('.post-slide');
+            currentPostIndex += direction;
+
+            // Verificar límites
+            if (currentPostIndex < 0) {
+                  currentPostIndex = posts.length - 1;
+            } else if (currentPostIndex >= posts.length) {
+                  currentPostIndex = 0;
+            }
+
+            // Ocultar todas las publicaciones y mostrar la actual
+            posts.forEach(post => post.style.display = 'none');
+            posts[currentPostIndex].style.display = 'block';
+         }
+      </script>
+
+   @endsection
 </x-geomir-layout>
 
 
