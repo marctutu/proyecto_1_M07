72d999502fa811ba95384abc3ffdae14163653b2
Its_Draky <axviro@fp.insjoaquimmir.cat>
2023-12-04 15:24:43 +0100

    layouts final

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -18,7 +18,7 @@
          <!-- Segundo menú con línea horizontal y desplazamiento hacia abajo -->
 
          <div class="bg-white-300 p-4 py-6 relative">
-            <div class="container mx-auto px-10 flex justify-between">
+            <div class="container mx-auto md:px-10 flex justify-between">
                <a href="#" class="px-4 hover:underline">Home</a>
                <a href="#" class="px-4 hover:underline">Explore</a>
                <a href="#" class="px-4 hover:underline">Chat</a>
@@ -33,12 +33,12 @@
 
          <!-- Contenido principal -->
 
-         <div class="w-full p-4 py-6 relative flex">
+         <div class="w-full p-4 py-6 relative md:flex">
 
             <!-- Imagen a la izquierda -->
 
-            <div class=" border-r-2 border-purple-800" style="width: 40%;">
-               <div id="place-container" class="place-container">
+            <div class="md:border-r-2 border-purple-800 md:w-5/12">
+               <div id="place-container" class="place-container border-purple-800 border-b-2 pb-5 mb-5 md:pb-0 md:mb-0 md:border-none ">
                   @forelse ($places as $index => $place)
                      <div onclick="openPlaceDetails('{{ route('places.show', $place->id) }}', event)" class="mr-6 place-slide" style="display: {{ $index === 0 ? 'block' : 'none' }}">                           <!-- Contenido de la publicación -->
                            <div>
@@ -69,16 +69,17 @@
 
             <!-- Contenedor de imágenes a la derecha -->
 
-            <div class="pl-8 border-r-2 border-purple-800" style="width: 50%;">
-               <!-- Fila 1 -->
-                  <div class="flex flex-wrap">
+            <div class=" md:pl-8 md:border-r-2 border-purple-800 md:w-6/12">
+               <div class=" border-purple-800 border-b-2 pb-5 mb-5 md:pb-0 md:mb-0 md:border-none">
+                  <!-- Fila 1 -->
+                  <div class="flex">
                      @forelse ($posts as $index => $post)
-                        <div class="w-1/4 mb-4 pr-11">
+                        <div class="w-1/3 md:mr-8 mb-4 pr-1 pl-1 md:pl-0 md:pl-0">
                            <div class= "border-8 rounded-md" style="border-color: #8A72AA;">
                               <div>
                                  @if($post->file)
                                     <div class="relative">
-                                          <img src='{{ asset("storage/{$post->file->filepath}") }}' alt="File Image" class="w-44 h-40">
+                                          <img src='{{ asset("storage/{$post->file->filepath}") }}' alt="File Image" class="w-40 h-40">
                                           <div class="border-2 rounded-3xl absolute top-0 m-2 left-0 flex flex-row" style="background-color: #D583F1; border-color: #8A72AA;">
                                              <img class="h-6" src='{{ asset("/img/user trans.png") }}'></img>
                                              <p class= "pb-1 pr-2">{{ $place->author->name }}</p>
@@ -87,11 +88,11 @@
                                  @endif
                               </div>
                               <div style="background-color: #ECCFFF;">
-                                 <p class="pl-1">{{ strlen($post->body) > 20 ? substr($post->body, 0, 20) . '...' : $post->body }}</p>
+                                 <p class="pl-1">{{ strlen($post->body) > 20 ? substr($post->body, 0, 15) . '...' : $post->body }}</p>
                               </div>
                            </div>
                         </div>
-               @if(($index + 1) % 4 == 0)
+               @if(($index + 1) % 3 == 0)
                   </div>
                   <div class="flex flex-wrap">
                @endif
@@ -99,16 +100,18 @@
                         <div class="w-full px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">No hay posts disponibles.</div>
                      @endforelse
                   </div>
+               </div>
             </div>
-            <div class="pl-8" style="width: 10%;">
-               <div class="container flex-col space-y-40">
-                  <div class="pt-10">
-                     <img class="h-16 pl-10" src='{{ asset("/img/likes morado fill.png") }}'></img>
+            
+            <div class="md:pl-8 md:w-1/12">
+               <div class="container flex md:flex-col justify-between md:space-y-40">
+                  <div class="md:pt-10">
+                     <img class="h-16 md:pl-10" src='{{ asset("/img/likes morado fill.png") }}'></img>
                   </div>
                   <div>
                      <img class="h-16 pl-8" src='{{ asset("/img/user trans.png") }}'></img>
                   </div>
-                  <div class="pb-10">
+                  <div class="md:pb-10">
                      <img class="h-16 pl-10" src='{{ asset("/img/favorites morado fill de verdad.png") }}'></img>
                   </div>
                   
